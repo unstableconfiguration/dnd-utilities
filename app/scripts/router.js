fec61215e432f0b23e22d21df403cc138b4aae28
oldmanmuffins <mluper.official@protonmail.com>
2021-08-31 17:05:40 -0400

    refining router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,13 +1,24 @@
+import { router } from 'lite'
 
 let main = document.getElementById('main-content');
 const load = (file) => new file.vm({ container : main });
 
-export let routes = [
+router.addRoutes([
     { route : '', value : () => { import('../pages/lookups/lookups.js').then(load); } },
     { route : 'lookups', value : () => { import('../pages/lookups/lookups.js').then(load); } },
     { route : 'battle-manager', value : () => { import('../pages/battle-manager/battle-manager.js').then(load); } },
     { route : 'encounter-builder', value : () => { import('../pages/encounter-builder/encounter-builder.js').then(load); } },
-]
+]);
 
+router.onHashChange = function(value) {
+    if(typeof(value) == 'function') { value(); }
+    else { throw new Exception("could not find value for route"); }
 
+    let navs = document.querySelectorAll('.nav-right > .nav-item')
+    navs.forEach(nav => {
+        let hash = location.hash || 'lookups'
+        let select = nav.querySelector('a').href.includes(hash);
+        nav.classList.toggle('active', select);
+    });
+}
 
--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 import 'cirrus-ui'; 
-import { routes } from './app/scripts/routes.js'
-import { router } from 'lite'
+import './app/scripts/router.js'
 import { header } from './app/header/header.js'
 
-window.router = router;
-router.addRoutes(routes);
-router.onHashChange = function(value) {
-    if(typeof(value) == 'function') { value(); }
-    else { throw new Exception("could not find value for route"); }
-
-    let navs = document.querySelectorAll('.nav-right > .nav-item')
-    navs.forEach(nav => {
-        let select = nav.querySelector('a').href.includes(location.hash);
-        nav.classList.toggle('active', select);
-    });
-}
 
 new header();
 
