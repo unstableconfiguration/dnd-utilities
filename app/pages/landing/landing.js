93f42fb13727d25dc5b4a56c3ed9f7eac22bc585
oldmanmuffins <mluper.official@protonmail.com>
2021-08-30 21:11:04 -0400

    adding gridify

--- app/pages/landing/landing.js
+++ app/pages/landing/landing.js
@@ -1,5 +1,15 @@
 import { lite } from 'lite';
+import { Gridify } from 'gridify';
 
 export let vm = lite.extend({
-    content : '<span>hello there</span>',
+    content : `
+        <span>hello there</span>
+        <div id='grid-container'></div>    
+    `,
+    initialize : function() { 
+        new Gridify({
+            container : 'grid-container',
+            data : [{ a : 1, b : 2 }]
+        })
+    }
 });
