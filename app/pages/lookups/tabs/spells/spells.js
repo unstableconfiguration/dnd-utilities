d3aa69ad423eb9e9092f3c6a2fad39bfadb5c73b
oldmanmuffins <mluper.official@protonmail.com>
2021-09-02 19:16:02 -0400

    finishing grid functionalizy

--- app/pages/lookups/tabs/spells/spells.js
+++ app/pages/lookups/tabs/spells/spells.js
@@ -2,6 +2,7 @@ import { lite } from 'lite';
 import html from './spells.html';
 import { spells } from '../../../../../5e/spells.js';
 import { Gridify } from 'gridify';
+import { pagination } from '../../pagination/pagination.js';
 
 export let table = lite.extend({
     content : html,
@@ -24,8 +25,8 @@ export let table = lite.extend({
             columns : [
                 { 
                     field : 'Name', 
-                    header : 'Name',
-                    //style : 'width:200px; text-align:left; text-decoration:underline;',
+                    header : "Name",
+                    style : 'width:200px; text-align:left; text-decoration:underline;',
                     sort : true,
                     filter : true,
                     click : (e) => {
@@ -38,21 +39,29 @@ export let table = lite.extend({
                         }).attach();
                     } 
                 },
-                { field : 'Level', header : 'Level', filter : true, sort : true }, // sort : true, style: 'width:50px; text-align:right;' },
-                { field : 'School', header : 'School', filter : true, sort : true }, // sort : true, style : 'width:125px'},
-                { field : 'CastingTime', header : 'Casting Time', filter : true, sort : true }, // sort:true, style: 'width:125px;' },
-                { field : 'Ritual', header : 'Ritual', filter : vm.getRitualFilter() }, // style : 'width:50px; text-align:center;'},
-                { field : 'Range', header : 'Range', filter : true, sort : true }, // sort : true, style: 'width:100px; overflow:hidden;' },
-                { field : 'Duration', header : 'Duration', filter : true, sort : true } // sort : true, style: 'width:100px;' } 
+                { field : 'Level', header : 'Level', filter : true, sort : true, style: 'width:100px;' },
+                { field : 'School', header : 'School', filter : true, sort : true, style : 'width:125px; text-align:left;'},
+                { field : 'CastingTime', header : 'Cast Time', filter : true, sort : true, style: 'width:150px; text-align:left;' },
+                { field : 'Ritual', header : 'Ritual', filter : vm.getRitualFilter(), style : 'width:50px;'},
+                { field : 'Range', header : 'Range', filter : true, sort : true, style: 'width:100px; text-align:left;' },
+                { field : 'Duration', header : 'Duration', filter : true, sort : true, style: 'width:100px;; text-align:left;' } 
             ],
-
-            //paging : true,
-            //style : 'table-layout:fixed; width:750px;',
-            className : 'table',
+            paging : { rows : 10 },
+            className : 'table small',
             onTableCellCreated(td, options) {
                 if(td.style.overflow === 'hidden') { td.title = td.innerText; }
+            },
+            onHeaderCreated(thead, options) {
+                thead.querySelectorAll('input[type="text"]')
+                    .forEach(i => i.className = "input-xsmall");
+            },
+            onHeaderCellCreated(th, options) { 
+                let sortIcon = th.querySelector('.sort');
+                if(sortIcon) { sortIcon.className = 'fa fa-sort'; } 
             }
-        })
+        });
+        let pageContainer = vm.grid.html.querySelector('#spells-grid-grid-paging');
+        new pagination({ container : pageContainer, grid : vm.grid, data : vm.grid.paging.data });
     },
     getRitualFilter : function() { 
         let vm = this;
