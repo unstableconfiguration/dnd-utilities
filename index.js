783e97e873cc0b4caf104dea4c742d7d3bf22cc9
oldmanmuffins <mluper.official@protonmail.com>
2021-09-02 19:18:43 -0400

    moving header to components

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'cirrus-ui'; 
 import './app/scripts/router.js'
-import { header } from './app/header/header.js'
+import { header } from './app/components/header/header.js'
 
 
 new header();
