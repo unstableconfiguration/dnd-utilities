7da40cde69eac6fbae4780010658e5e6658cb50c
oldmanmuffins <mluper.official@protonmail.com>
2021-09-03 11:42:38 -0400

    adding pagination, fixing filters

--- app/pages/lookups/tabs/monsters/monsters.js
+++ app/pages/lookups/tabs/monsters/monsters.js
@@ -1,6 +1,7 @@
 import { Gridify } from 'gridify';
 import { lite } from 'lite';
 import { monsters } from '../../../../../5e/monsters';
+import { pagination } from '../../pagination/pagination'
 
 export let table = lite.extend({
     content : '<div id="monsters-grid">Test 2</div>', 
@@ -17,14 +18,14 @@ export let table = lite.extend({
     },
     buildGrid : function(monsterArray) { 
         let vm = this;
-        new Gridify({
+        vm.grid = new Gridify({
             container : 'monsters-grid',
             data : monsterArray,
             columns : [
                 {
                     field : 'Name',
                     header : 'Name',
-                    filter : { rule : vm.nameFilter },
+                    filter : vm.nameFilter,
                     className : 'td-text-long',
                     style : 'width: 200px; text-decoration:underline',
                     sort : true,
@@ -34,19 +35,35 @@ export let table = lite.extend({
                 }
                 , { field : 'ChallengeRating', header : 'CR', 
                     style : 'width:75px; text-align:right;',
-                    filter : { rule : vm.crFilter }, 
-                    sort : { comparator : vm.crSort }
+                    filter : vm.crFilter, 
+                    sort : vm.crSort
                 }
                 , { field : 'Type', header : 'Type',
                     className : 'td-text-long',
                     style : 'width: 100px;',
                     filter : true, format : (v)=> { return v.split(',')[0]; }, sort : true }
-                , { field : 'Alignment', header : 'Alignment', style : 'width:100px; text-align:left;', filter : true, sort : true }
-            ]
-            , paging : { rows : 10 }
-            , style : 'table-layout:fixed;'
-            , className : 'table small'      
+                , { field : 'Alignment', header : 'Alignment', style : 'width:100px;', className : 'td-text-long', filter : true, sort : true }
+            ],
+            paging : { rows : 10 },
+            style : 'table-layout:fixed;',
+            className : 'table small',
+            onTableCellCreated(td, options) {
+                if(td.style.overflow === 'hidden') { 
+                    td.title = td.innerText; 
+                }
+            },
+            onHeaderCreated(thead, options) {
+                thead.querySelectorAll('input[type="text"]')
+                    .forEach(i => i.className = "input-xsmall");
+            },
+            onHeaderCellCreated(th, options) { 
+                let sortIcon = th.querySelector('.sort');
+                if(sortIcon) { sortIcon.className = 'fa fa-sort'; } 
+            }    
         });
+        let pageContainer = vm.grid.html.querySelector('#monsters-grid-grid-paging');
+        new pagination({ container : pageContainer, grid : vm.grid, data : vm.grid.paging.data });
+    
     },
     crSort : function(a, b) {
         let parse = (cr) => cr.indexOf('/') === -1 ? +cr : 1/(cr.split('/')[1]); 
@@ -56,7 +73,7 @@ export let table = lite.extend({
         return a > b ? 1 : -1;
     },
     crFilter : function(cellValue, filterValue) {
-        if(+filterValue === 1) { return +cell_value === 1; }
+        if(+filterValue === 1) { return +cellValue === 1; }
         return ('' + cellValue).substr(0, filterValue.length) === filterValue; 
     },
     nameFilter : function(cellValue, filterValue) { 
