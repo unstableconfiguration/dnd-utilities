b04530bac27c3a8f45d0977ca8b4900a6ab9a45d
oldmanmuffins <mluper.official@protonmail.com>
2021-09-04 10:39:05 -0400

    renaming controls -> elements

--- app/pages/lookups/pagination/pagination.js
+++ app/pages/lookups/pagination/pagination.js
@@ -5,45 +5,45 @@ export let pagination = lite.extend({
     content : html,
     initialize : function() {
         let vm = this;
-        vm.setControls();
+        vm.setElements();
         vm.addEventListeners();
         
         // After filtering, reset page count
         let filter = this.grid.filters.filter;
         this.grid.filters.filter = function(...args) { 
             filter(...args);
-            vm.updateControls();
+            vm.updateElements();
         }
         
-        vm.updateControls();
+        vm.updateElements();
     },
-    setControls : function() { 
+    setElements : function() { 
         let vm = this;
-        vm.controls = {
+        vm.elements = {
             btnPrev : vm.container.querySelector('#btnPrev'),
             btnNext : vm.container.querySelector("#btnNext"),
             display : vm.container.querySelector('#pagination-text')
         };
     },
     addEventListeners : function() { 
-        this.controls.btnPrev.addEventListener('click', this.onPrevButtonClick.bind(this));
-        this.controls.btnNext.addEventListener('click', this.onNextButtonClick.bind(this))
+        this.elements.btnPrev.addEventListener('click', this.onPrevButtonClick.bind(this));
+        this.elements.btnNext.addEventListener('click', this.onNextButtonClick.bind(this))
     },
     onPrevButtonClick : function(e) { 
         this.data.currentPage--;
         this.grid.page(this.data.currentPage);
         
-        this.updateControls();
+        this.updateElements();
     },
     onNextButtonClick : function(e) { 
         this.data.currentPage++;
         this.grid.page(this.data.currentPage);
 
-        this.updateControls();
+        this.updateElements();
     },
-    updateControls : function() { 
-        this.controls.btnPrev.classList.toggle("disabled", this.data.currentPage == 1);
-        this.controls.btnNext.classList.toggle("disabled", this.data.currentPage == this.data.visiblePages);
-        this.controls.display.innerHTML = "Page " + this.data.currentPage + " of " + this.data.visiblePages;
+    updateElements : function() { 
+        this.elements.btnPrev.classList.toggle("disabled", this.data.currentPage == 1);
+        this.elements.btnNext.classList.toggle("disabled", this.data.currentPage == this.data.visiblePages);
+        this.elements.display.innerHTML = "Page " + this.data.currentPage + " of " + this.data.visiblePages;
     }
 });
