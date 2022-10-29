2b3b4c772f71ea98be451cc668f6df94a4eb1809
oldmanmuffins <mluper@protonmail.com>
2022-10-29 13:46:02 -0400

    loading 'card' ui elements

---
+++ app/components/card/card.js
@@ -0,0 +1,34 @@
+import html from './card.html'
+import { Lite } from 'lite'
+
+export class Card { 
+    constructor(options) {
+        this.container = Lite.append(options.container, html);
+
+        this.image = options.image;
+        this.title = options.title;
+        this.body = options.body;
+
+        this.bindData();
+    }
+
+    get ui()  {
+        let card = this;
+        return { 
+            get image() { return card.container.querySelector('.card__image'); },
+            get title() { return card.container.querySelector('.tile__title'); },
+            get body() { return card.container.querySelector('.card__body'); }
+        }
+    }
+    
+    bindData() {
+        let proxy = Lite.bindings.bind(this.container, {
+            title : this.title,
+            body : this.body
+        });
+        if(this.image) {
+            this.ui.image.style.backgroundImage = `url("${this.image}")`;
+        }
+    }
+
+}
--- app/pages/character-sheets/character-sheets.js
+++ app/pages/character-sheets/character-sheets.js
@@ -1,10 +1,40 @@
 import html from './character-sheets.html'
 import { Lite } from 'lite';
+import { Card } from '../../components/card/card.js'
 
 export class CharacterSheets {
     constructor(options) { 
         this.container = Lite.append(options.container, html);
+
+        let test = this.container.querySelector('#test');
+        new Card({
+            container : test,
+            body : 'body test',
+            title : 'title test',
+            image : 'https://picsum.photos/id/237/200/300/?blur=2'
+        })
+
+       let test2 = this.container.querySelector('#test2');
+        new Card({
+            container : test2,
+            body : 'body test 2',
+            title : 'title test 2'
+        })
     }
+
+    // we need a way to show names to click on
+    // we could do a tab system to be consistent
+    // I'd want that to be a potential carousel though or something similar.
+
+
+    // lets use tabs and we can adjust it if/when we need more items
+
+    // or we can do cards for more visual appeal
+
+
+    // cards are nice visually, but how do we represent it
+    // we can load the cards, clicking on one does a redirect
+    // yeah that's fine
 
 }
 export let View = CharacterSheets;
