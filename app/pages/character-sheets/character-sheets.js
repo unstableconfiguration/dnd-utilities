aeac4fabcbd219e1d98979ddbeefc3938d69abbe
oldmanmuffins <mluper@protonmail.com>
2022-10-30 19:33:28 -0400

    dynamic card generation

--- app/pages/character-sheets/character-sheets.js
+++ app/pages/character-sheets/character-sheets.js
@@ -2,38 +2,49 @@ import html from './character-sheets.html'
 import { Lite } from 'lite';
 import { Card } from '../../components/card/card.js'
 
+let characters = [
+    {
+        name : 'One Trick',
+        description : 'description of one trick',
+        url : '#character-sheet/one-trick'
+    }
+];
+
+
+
 export class CharacterSheets {
     constructor(options) { 
         this.container = Lite.append(options.container, html);
 
-        // for route
-        // #character-sheets from top nav
-        // #character-sheet/{name}
-
-        let test = this.container.querySelector('#test');
-        new Card({
-            container : test,
-            body : 'body test',
-            title : 'title test',
-            image : 'https://picsum.photos/id/237/200/300/?blur=2',
-            click() { window.location.hash = '#character-sheet/barbican-brady' }
-        })
+        this.#addCards();
 
     }
 
-    // we need a way to show names to click on
-    // we could do a tab system to be consistent
-    // I'd want that to be a potential carousel though or something similar.
+    #addCards() {
+        for(let i in characters) {
+            let div = this.#getDiv(i);
+            let character = characters[i];
+            new Card({
+                container : div,
+                title : character.name,
+                body : character.description,
+                image : character.image, 
+                click() { window.location.hash = character.url; }
+            })
+        }
 
+    }
 
-    // lets use tabs and we can adjust it if/when we need more items
-
-    // or we can do cards for more visual appeal
-
-
-    // cards are nice visually, but how do we represent it
-    // we can load the cards, clicking on one does a redirect
-    // yeah that's fine
+    #getDiv(i) {
+        let cardContainer = this.container.querySelector('#card-container');
+        cardContainer.insertAdjacentHTML('beforeend', `
+            <div class='col-4'>
+                <div id='card-${i}'>
+                </div>
+            </div>
+        `);
+        return cardContainer.querySelector(`#card-${i}`)
+    }
 
 }
 export let View = CharacterSheets;
